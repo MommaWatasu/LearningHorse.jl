0ca1195de77ecf4bbd31269c9b3f9aeda093e53a
QGMW22 <76277264+QGMW22@users.noreply.github.com>
2021-01-11 14:48:10 +0900

    Implemented multiple regression (operation check is in progress)

--- Horse.jl
+++ Horse.jl
@@ -1,9 +1,9 @@
 module Horse
 
-    export Simple_Regression
-    module Simple_Regression
-        export GSM
-        function GSM(x, t, alpha = 0.001, tau_max = 100000, eps = 0.1) #this is Gradient steepest method
+    export Linear_Regression
+    module Linear_Regression
+        export SGD
+        function SGD(x, t, alpha = 0.001, tau_max = 100000, eps = 0.1) #this is縲Slope縲Gradient Descent
             function DMSE(x,t,w)
                 if length(x) != length(t)
                     throw("The sizes of the arguments x and t you passed do not match.")
@@ -31,20 +31,39 @@ module Horse
             w1 = w_hist[a, 2]
             return w0, w1
         end
+        module MR()
+　　　　　　function fit(x, t)
+                try
+                    w = nothing
+                    x2 = zeros(size(x)[1],1)
+                    x = hcat(x2, x)
+                    w = inv(x' * x) * x' * t
+                    return w
+                catch
+                    println("Perhaps the matrix x you passed does not have an inverse matrix.
+In that case, use ridge regression.")
+                end
+            end
+            function predict(x, w)
+                x2 = zeros(size(a)[1],1)
+                x = hcat(x2, x)
+                return x * w
+            end
+        end
     end
 
-  export Loss_Function
-  module Loss_Function # this is Mean Square error
-    export MSE
-    function MSE(x, t, w)
-      if length(x) != length(t)
-        throw("The sizes of the arguments x and t you passed do not match.")
-      end
-      y = w[1] * x .+ w[2]
-      mse = sum((y - t) .^ 2) / length(y) #mse is Mean Square error
-      return mse
+    export Loss_Function
+    module Loss_Function # this is Mean Square error
+        export MSE
+        function MSE(x, t, w)
+        if length(x) != length(t)
+            throw("The sizes of the arguments x and t you passed do not match.")
+        end
+        y = w[1] * x .+ w[2]
+        mse = sum((y - t) .^ 2) / length(y) #mse is Mean Square error
+        return mse
+        end
     end
-  end
 # Write your package code here.
 
 end
