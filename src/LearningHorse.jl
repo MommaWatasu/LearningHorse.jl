9faaea2507886680be080a7f151e30a467478cc0
QGMW22 <76277264+QGMW22@users.noreply.github.com>
2021-02-10 22:16:41 +0900

    Implement polynomial regression.

--- src/LearningHorse.jl
+++ src/LearningHorse.jl
@@ -121,12 +121,15 @@ In that case, use ridge regression.")
                 return w' * x
             end
         end
-        module GBFM() #this use Gaussian basis function model for regression
+        module BFM() #this use Basis Function Model for regression
             using LinearAlgebra
             function gauss(x, m, s)
                 return exp.(-(x .- m) .^ 2 ./ (2 * s ^ 2))
             end
-            function fit(x, t, M; m = [], s = nothing, alpha = 0.0)
+            function polynomial(x, i)
+                return x .^ i
+            end
+            function fit(x, t, M; m = [], s = nothing, alpha = 0.0, f = "gauss")
                 if m == []
                     ma, mi = round.(Int, (maximum(x), minimum(x)))
                     inter = (ma - mi) / (M - 1)
@@ -134,7 +137,6 @@ In that case, use ridge regression.")
                         push!(m, mi)
                         mi += inter
                     end
-                    println(m)
                 end
                 if s == nothing
                     s = inter
@@ -143,14 +145,20 @@ In that case, use ridge regression.")
                     x = x'
                     t = t'
                 end
-                i = Matrix{Float64}(I, size(x)[2], size(x)[2])
+                i = Matrix{Float64}(I, M + 1, M + 1)
                 try
                     phi = ones(size(x)[2], M + 1)
-                    for j in 1 : M
-                        phi[:, j] = gauss(x, m[j], s)
+                    if f == "gauss"
+                        for j in 1 : M
+                            phi[:, j] = gauss(x, m[j], s)
+                        end
+                    elseif f == "polynomial"
+                        for j in 1 : M
+                            phi[:, j] = polynomial(x, j - 1)
+                        end
                     end
                     phit = phi'
-                    b = inv(phit * phi)
+                    b = inv(phit * phi + i * alpha)
                     c = b * phit
                     w = c * t
                     return w, m, s
@@ -158,11 +166,17 @@ In that case, use ridge regression.")
                     println("Perhaps the matrix x you passed does not have an inverse matrix. In that case, you should add the 'alpha' to the arguments('alpha' is a regularization term)")
                 end
             end
-            function predict(x, w, m, s)
+            function predict(x, w, m, s; f = "gauss")
                 w_len = length(w)
                 t = fill!(copy(x), 0)
-                for i in 1 : w_len - 1
-                    t += w[i] * gauss(x, m[i], s)
+                if f == "gauss"
+                    for i in 1 : w_len - 1
+                        t += w[i] * gauss(x, m[i], s)
+                    end
+                elseif f == "polynomial"
+                    for i in 1 : w_len - 1
+                        t += w[i] * polynomial(x, i - 1)
+                    end
                 end
                 t .+= w[w_len]
                 return t
@@ -472,18 +486,29 @@ In that case, use ridge regression.")
     export LossFunction
     module LossFunction #Loss Fuunctions 
         using LinearAlgebra
-        function MSE(x, t, w; gauss = false, m = nothing, s = nothing, mean_f = true) # this is Mean Square Error
+        function MSE(x, t, w; b = "", m = nothing, s = nothing, mean_f = true) # this is Mean Square Error
             function gauss_func(x, m, s)
                 return exp.(-(x .- m) .^ 2 ./ (2 * s ^ 2))
+            end
+            function polynomial_func(x, i)
+                return x .^ i
             end
             if length(x) != length(t)
                 throw("The sizes of the arguments x and t you passed do not match.")
             end
-            if gauss
+            if b != ""
                 w_len = length(w)
                 y = fill!(copy(x), 0)
-                for i in 1 : w_len - 1
-                    y += w[i] * gauss_func(x, m[i], s)
+                if b == "gauss"
+                    for i in 1 : w_len - 1
+                        y += w[i] * gauss_func(x, m[i], s)
+                    end
+                elseif b == "polynomial"
+                    for i in 1 : w_len - 1
+                        println("size", size(t), size(polynomial_func(x, i - 1)))
+                        println("w[i] * polynomial_func(x, i - 1)", w[i] * polynomial_func(x, i - 1))
+                        t += (w[i] * polynomial_func(x, i - 1))'
+                    end
                 end
                 y .+= w[w_len]
                 if size(t)[1] != size(y)[1]
